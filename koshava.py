7f56d799e67d6916049ee90f428eb83166926338
Oliver Steffen <steffen@kph.uni-mainz.de>
2014-09-22 14:47:44 +0200

    class:

--- koshava.py
+++ koshava.py
@@ -5,19 +5,41 @@ import sys
 import pdb
 import array
 import time
-
+import struct
 
 class Koshava:
+
+    # USB IDs of the Koshava 5
     VENDOR_ID = 0x21c5
     PRODUCT_ID = 0x03e8
+
+    # USB connection __handlers
     device = None
     ep_in = None
     ep_out = None
-    units = ['T','G','V/cm','KV/m','Oe' ]
 
+    # static data
+    units = ['T', 'G', 'V/cm', 'KV/m', 'Oe' ]   # Unit names
+    convT = [ 1.0, 1.0/10 ,1.0/(10*1000), 1.0/(100*1000), 1.0 ] # Conversion factors to Tesla for each range
+    rangetxt = [ 'Invalid', '2T', '200mT', '20mT', 'Invalid' ]  # Range names
+
+    # Probe state values
+    probeConnected = False
+    rangepos = 0
+    adc = 0
+    unit = 0
+    acdc = 0
+    autorange = False
+    temp = 0
+    B = 0
+    minB = 0
+    maxB = 0
+
+    """Check if the USB connection is established"""
     def isConnected(self):
         return (self.device is not None and self.ep_in is not None and self.ep_out is not None)
 
+    """Connect to the device"""
     def Connect(self):
         
         self.device = usb.core.find(idVendor=self.VENDOR_ID,
@@ -58,8 +80,8 @@ class Koshava:
 
         if self.ep_out is None:
            raise ValueError('Endpoint OUT not found') 
-        else:
-            print "Endpoint OUT found @", self.ep_out.bEndpointAddress
+       #else:
+       #     print "Endpoint OUT found @", self.ep_out.bEndpointAddress
 
         self.ep_in = usb.util.find_descriptor(
             intf,
@@ -71,133 +93,181 @@ class Koshava:
 
         if self.ep_in is None:
            raise ValueError('Endpoint IN not found') 
-        else:
-            print "Endpoint IN found @", self.ep_in.bEndpointAddress
+       #else:
+        #    print "Endpoint IN found @", self.ep_in.bEndpointAddress
+
+        # confiure probe with default settings
+        self.setValues()
 
         return True
 
-    def write(self, msg):
-        #print "Write:", msg
+    """Write a binary message to the probe"""
+    def __write(self, msg):
         try:
             self.ep_out.write(msg)
         except usb.core.USBError,e:
             print "WRITE:", e
             pass
 
-    def read(self):
+    """Read from the device"""
+    def __read(self):
         try:
             m = self.ep_in.read(64)
-            #print "Read:",m
             return m
         except usb.core.USBError, e:
             print "READ:", e
             return None
 
-    def makeMessage(self, cmd):
+    def __makeMessage(self, cmd):
         msg = bytearray(64)
         msg[0] = chr(cmd)
         return msg
 
-    def bytes2short(self, bts):
-        return bts[1]*256+bts[0]
+    def __bytes2short(self, bts):
+        return struct.unpack('h', self.__array2string(bts))[0]
 
-    def handle1D(self, msg):
-        adc_ = msg[2:4]
-        adc = self.bytes2short(adc_)
-        rangepos = msg[4]
-        unit = msg[5]
-        acdc = msg[6]
-        autorange = (msg[13] == 1)
-        temp_ = msg[14:16]
-        temp = self.bytes2short(temp_)/10.0
-        minadc = self.bytes2short(msg[9:11])
-        maxadc = self.bytes2short(msg[11:13])
+    def __handle1D(self, msg):
+        self.adc = self.__bytes2short(msg[2:4])
+        self.rangepos = msg[4]
+        self.unit = msg[5]
+        self.acdc = msg[6]
+        self.autorange = (msg[13] == 1)
+        self.temp = self.__bytes2short(msg[14:16]) / 10.0
+        self.minB = self.__bytes2short(msg[ 9:11]) * self.convT[self.rangepos]
+        self.maxB = self.__bytes2short(msg[11:13]) * self.convT[self.rangepos]
+        self.B = self.adc * self.convT[ self.rangepos ]
 
-        B = 1.0 * adc / 10**(rangepos)
+    def __handle32(self, msg):
+        self.probeConnected = (msg[1] == 1)
 
-        print "Unit: ", unit, self.units[unit]
-        print "AC/DC:" , acdc
-        print "adc:",adc
-        print "autorange:",autorange
-        print "Temp:",temp
-        print "RangePos:",rangepos
-        print " B =",B,self.units[unit]
-        print "min/max", minadc, maxadc
-
-
-    def handle32(self, msg):
-        probe_connected = (msg[1] == 1)
-        print "Probe is connected?", probe_connected
-
-    def array2string(self, arr):
+    def __array2string(self, arr):
         s = "".join(chr(b) for b in arr) 
         return s
 
-    def handle1C(self,msg):
-        devicename = self.array2string(msg[2:22])
-        deviceno = msg [22:31]
-        probename = self.array2string(msg [31:53])
-        probecalibdate= msg [53:57]
-        probeno = msg [57:62]
-        print "Device name:", devicename, "Number:", deviceno
-        print "Probe name:", probename, "Number:", probeno
-        print "Calibrated:", probecalibdate
+    def __handle1C(self,msg):
+        self.devicename = self.__array2string(msg[2:22])
+        self.deviceno = msg[22:31]
+        self.probename = self.__array2string(msg[31:53])
+        self.probecalibdate= msg[53:57]
+        self.probeno = msg[57:62]
 
-    def handleALL(self, msg):
+    def __handleALL(self, msg):
         cmd = msg[0]
 
         if(cmd == 0x1D):
-            self.handle1D(msg)
+            self.__handle1D(msg)
         elif(cmd == 0x32):
-            self.handle32(msg)
+            self.__handle32(msg)
         elif(cmd == 0x1c):
-            self.handle1C(msg)
+            self.__handle1C(msg)
         elif(cmd == 0x2a):
-            self.handle2a(msg)
+            self.__handle2a(msg)
 
-    def sendcmd(self, cmd, answer=True):
-        msg = self.makeMessage(cmd)
-        self.write(msg)
+    def __sendcmd(self, cmd, answer=True):
+        msg = self.__makeMessage(cmd)
+        self.__write(msg)
 
         if( answer ):
-            reply = self.read()
+            reply = self.__read()
             if( reply is not None ):
-                self.handleALL(reply)
+                self.__handleALL(reply)
             else:
                 print "No reply!"
 
     def setValues(self, vrange=3, unit=0, acdc=0, lang=0, autorange=False):
-        msg = self.makeMessage(0x2a)
+        msg = self.__makeMessage(0x2a)
         msg[1] = chr(0x2a)
         msg[4] = chr(vrange)
         msg[5] = chr(unit)
         msg[6] = chr(acdc)
         msg[7] = chr(lang)
         msg[13] = chr( 1 if autorange else 0 )
-        self.write(msg)
+        self.__write(msg)
 
-        reply = self.read()
+        reply = self.__read()
         if( reply is not None ):
-            self.handleALL(reply)
+            self.__handleALL(reply)
         else:
             print "No reply!"
 
 
-    def handle2a(self, msg):
+    def __handle2a(self, msg):
         print msg
+
+
+    """Set auto-ranging. returns True if on, False if off"""
+    def SetAutorange(self, state=True):
+        self.setValues(vrange=self.rangepos, unit=self.unit, acdc=self.acdc, lang=0, autorange=True)
+        return self.autorange
+
+    """Check if auto-ranging is on (True) or off (False)"""
+    def GetAutorange(self):
+        return self.autorange
+
+    """Set the measuring range (1..4)"""
+    def SetRange(self, r=0):
+        self.setValues(vrange=r, unit=self.unit, acdc=self.acdc, lang=0, autorange=self.autorange)
+        return self.rangepos
+
+    """Get the current measuring range as number (1..4)"""
+    def GetRange(self):
+        return self.rangepos
+
+    """Get the current measuring range as text name (e.g. 20mT)"""
+    def GetRangeTxt(self):
+        return self.rangetxt[self.rangepos]
+
+    """Set measuring mode to AC. Returns True on success"""
+    def SetAC(self, ac=True):
+        self.setValues(vrange=self.rangepos, unit=self.unit, acdc=ac, lang=0, autorange=self.autorange)
+        return self.acdc
+
+    """Check if measureing mode is AC (True) or DC (False)"""
+    def GetAC(self):
+        return self.acdc
+
+    """Get the probe name"""
+    def GetProbeName(self):
+        return self.probename
+
+    """Get the device name"""
+    def GetDeviceName(self):
+        return self.devicename
+
+    """Read data from the device. Returns (B, B_min, B_max)"""
+    def ReadData(self):
+        self.__sendcmd(0x1d)
+        return self.B, self.minB, self.maxB
+
+    """Check if a hall probe is connected to the device"""
+    def ProbeConnected(self):
+        self.__sendcmd(0x32)
+        return self.probeConnected
+
+    """Get measured field in Tesla"""
+    def GetB(self):
+        return self.B
+
+    """Get probe temperature in Celsius"""
+    def GetTemp(self):
+        return self.temp
 
 
 myKoschava = Koshava()
 
-myKoschava.Connect()
-#time.sleep(.5)
-#myKoschava.sendcmd(0x1C)
-#time.sleep(.5)
-#myKoschava.sendcmd(0x32)
-# time.sleep(.5)
-# myKoschava.sendcmd(0X1E, answer=False)
+if not myKoschava.Connect():
+    print "Could not connect to USB device!"
+    sys.exit(1)
+
+if not myKoschava.ProbeConnected():
+    print "No hall probe connected to device!"
+    sys.exit(2)
+
 while(True):
-    myKoschava.sendcmd(0x1D)
+    B, mi, ma = myKoschava.ReadData()
+
+    print "%0.5f, %0.5f, %0.5f" % (B, mi, ma)
+
     time.sleep (.5)
 
 #myKoschava.setValues(unit=0, vrange=3, autorange=True)
